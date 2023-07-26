0a2cfd90e4af1cffa58406cda7ecb162c6699b49
Alvin Joy <89687635+AlvinJoyDev@users.noreply.github.com>
2023-07-26 04:02:30 +0000

    Update copoint.c

--- copoint.c
+++ copoint.c
@@ -1,1 +1,16 @@
-hi
+#include <stdio.h>
+struct point
+{
+	int x;
+	int y;
+} p[2];
+void main()
+{
+	printf("Enter point 1: ");
+	scanf("%d %d",&p[0].x,&p[0].y);
+	printf("Enter second point: ");
+	scanf("%d %d",&p[1].x,&p[1].y);
+	p[2].x=p[0].x+p[1].x;
+	p[2].y=p[0].y+p[1].y;
+	printf("The new point is %d %d",p[2].x,p[2].y);   
+}
