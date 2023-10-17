944dc4eaf9be7d4277b18a7a086edc467049a6b2
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-10-17 15:34:07 +0530

    Update sparse.c

--- dsa/sparse.c
+++ dsa/sparse.c
@@ -25,21 +25,21 @@ void main()
 	if(size>=(row*col/2))
 	{
 		printf("It is a sparse matrix\n");
-		int compactMatrix[3][size];
+		int compactMatrix[size][3];
     	int k=0;
     	for (int i=0;i<row;i++)
         	for (int j=0;j<col;j++)
             	if (a[i][j]!=0)
             	{
-                	compactMatrix[0][k]=i;
-                	compactMatrix[1][k]=j;
-                	compactMatrix[2][k]=a[i][j];
+                	compactMatrix[k][0]=i;
+                	compactMatrix[k][1]=j;
+                	compactMatrix[k][2]=a[i][j];
                 	k++;
             	}
  
-    	for (int i=0;i<3;i++)
+    	for (int i=0;i<size;i++)
     	{
-        	for (int j=0;j<size;j++)
+        	for (int j=0;j<3;j++)
             	printf("%d ",compactMatrix[i][j]);
         	printf("\n");
     	}
