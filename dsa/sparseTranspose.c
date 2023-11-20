80c0b2a888fb4af0151179e9100db7a64a68abaa
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-11-20 20:34:11 +0530

    Update

--- dsa/sparseTranspose.c
+++ dsa/sparseTranspose.c
@@ -5,21 +5,27 @@ void main()
 	int spmatrix[100][3];
 	int transpose[100][3];
 	int i,j,k,row,col;
-	printf("Enter the order of sparse matrix\n");
+	printf("Enter the order of sparse matrix:\n");
 	scanf("%d%d",&row,&col);
-	printf("Enter the element of the sparse matrix\n");
+	printf("Enter the elements of the sparse matrix:\n");
 	for(i=0;i<row;i++)
-	for(j=0;j<col;j++)
-		scanf("%d",&matrix[i][j]);
+	{
+		for(j=0;j<col;j++)
+			scanf("%d",&matrix[i][j]);
+	}
 	k=1;
 	for(i=0;i<row;i++)
-	for(j=0;j<col;j++)
-	if(matrix[i][j]!=0)
 	{
-		spmatrix[k][0]=i;
-		spmatrix[k][1]=j;
-		spmatrix[k][2]=matrix[i][j];
-		k++;
+		for(j=0;j<col;j++)
+		{
+			if(matrix[i][j]!=0)
+			{
+				spmatrix[k][0]=i;
+				spmatrix[k][1]=j;
+				spmatrix[k][2]=matrix[i][j];
+				k++;
+			}
+		}
 	}
 	spmatrix[0][0]=row;
 	spmatrix[0][1]=col;
@@ -37,13 +43,17 @@ void main()
 	transpose[0][2]=spmatrix[0][2];
 	k=1;
 	for(i=0;i<spmatrix[0][1];i++)
-	for(j=0;j<=spmatrix[0][2];j++)
-	if(spmatrix[j][1]==i)
 	{
-		transpose[k][0]=spmatrix[j][1];
-		transpose[k][1]=spmatrix[j][0];
-		transpose[k][2]=spmatrix[j][2];
-		k++;
+		for(j=0;j<=spmatrix[0][2];j++)
+		{
+			if(spmatrix[j][1]==i)
+			{
+				transpose[k][0]=spmatrix[j][1];
+				transpose[k][1]=spmatrix[j][0];
+				transpose[k][2]=spmatrix[j][2];
+				k++;
+			}
+		}
 	}
 	printf("Transpose of the sparse matrix\n");
 	for(i=0;i<=transpose[0][2];i++)
