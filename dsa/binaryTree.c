fd65c9a98c5d6d08c30cfa3523a285e5655b6eca
Alvin Joy <89687635+alvinsjoy@users.noreply.github.com>
2023-12-09 16:18:11 +0530

    Refactor binary tree traversal and add insert functionality

--- dsa/binaryTree.c
+++ dsa/binaryTree.c
@@ -49,14 +49,38 @@ void postorder(struct node* root) {
     printf("%d ", root->data);
 }
 
-void main{
-	int data,ch;
-	while(1){
-		printf("\n1.Insert\n2.Inorder traversal\n3.Preorder traversal\n4.Postorder traversal\n5.Exit\nEnter your choice: ");
-		scanf("%d",&ch);
-		switch(ch){
-		case 1:
-			
+void main() {
+    struct node* root = NULL;
+    int data, ch;
+    while(1){
+        printf("\n1.Insert\n2.Inorder traversal\n3.Preorder traversal\n4.Postorder traversal\n5.Exit\nEnter your choice: ");
+        scanf("%d", &ch);
+        switch(ch) {
+            case 1:
+                printf("Enter the data to be inserted: ");
+                scanf("%d", &data);
+                root = insertNode(root, data);
+                break;
+			case 2:
+				printf("Inorder traversal: ");
+            	inorder(root);
+            	printf("\n");
+            	break;
+        	case 3:
+            	printf("Preorder traversal: ");
+            	preorder(root);
+            	printf("\n");
+            	break;
+        	case 4:
+            	printf("Postorder traversal: ");
+            	postorder(root);
+            	printf("\n");
+            	break;
+        	case 5:
+            	exit(0);
+        	default:
+            	printf("Invalid choice\n");
 		}
 	}
 }
+
